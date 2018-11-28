deca35d72a38f28259b6df5ca9b3da23283592d2
Dmitry <hominsky99@gmail.com>
2018-11-28 10:04:50 +0300

    Add files via upload

---
+++ GetSave.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEngine.UI;
+
+public class GetSave : MonoBehaviour {
+	
+	private GameObject GoldController;
+    private GameObject Monster;
+    private GameObject Upgrade;
+    private string _savePath; //путь с сохранением
+    private SavingInfo _infoFromSavedFile; // класс с переменными для сохраняемой/получаемой инфы
+
+	//
+	private GameObject Upgrade1;
+	//
+
+	void Start () {
+        try
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        _savePath = Path.Combine(Application.persistentDataPath, "arpgsave.json"); // внешняя память -> Android -> Data -> название бандла проекта -> files
+#else
+            _savePath = Path.Combine(Application.dataPath, "arpgsave.json");
+#endif
+            if (File.Exists(_savePath))
+            {
+				GoldController = GameObject.Find("GoldController");
+                Monster = GameObject.Find("Chan1");
+                Upgrade = GameObject.Find("DamageController");
+
+                _infoFromSavedFile = JsonUtility.FromJson<SavingInfo>(File.ReadAllText(_savePath)); //пишем инфу из файла в _infoFromSavedFile
+
+                GoldController.GetComponent<GoldController>().Set_Player_Gold(_infoFromSavedFile.Gold); // присваиваем playerGold значение экземпляра класса _infoFromSavedFile
+                //GoldController.GetComponent<GoldController>().update_PlayerGold();
+
+                Monster.GetComponent<Monster>().CurrentHP = _infoFromSavedFile.CurrentHP;
+                Monster.GetComponent<Monster>().MaxHP = _infoFromSavedFile.MaxHP;
+                Monster.GetComponent<HPController>().Update_HP_Bar_and_Text();
+				Monster.GetComponent<SpriteRenderer>().sprite = _infoFromSavedFile.Sprite;
+				Upgrade.GetComponent<DamageController>()._totalBoost = _infoFromSavedFile.boost;
+
+					Upgrade1 = GameObject.Find("Boost_0");
+					Upgrade1.GetComponent<OnClick_Buy_Upgrade>().Set_BoostCount(Upgrade.GetComponent<DamageController>()._totalBoost[0]);
+					Debug.Log("Boost_0" + ": Значение= " + Upgrade.GetComponent<DamageController>()._totalBoost[0] + "  загружено из файла");
+				Upgrade.GetComponent<DamageController>().Refresh_ClickDamage();
+
+					Upgrade1 = GameObject.Find("Boost_1");
+					Upgrade1.GetComponent<OnClick_Buy_Upgrade>().Set_BoostCount(Upgrade.GetComponent<DamageController>()._totalBoost[1]);
+					Debug.Log("Boost_1" + ": Значение= " + Upgrade.GetComponent<DamageController>()._totalBoost[1] + "  загружено из файла");
+				Upgrade.GetComponent<DamageController>().Refresh_AutoDamage();
+			}
+        }
+        catch(Exception ex)
+        {
+            Debug.Log("ERROR " + ex);
+        }
+	}
+}
---
+++ Save.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEngine.UI;
+
+public class Save : MonoBehaviour {
+
+	private GameObject GoldController;
+    private GameObject Monster;
+    private GameObject Upgrade;
+    private SavingInfo _savingInfo = new SavingInfo();
+    private string _savingPath;
+    private string json;
+
+#if UNITY_ANDROID && !UNITY_EDITOR // проверка сохраняем с андроида или нет
+    private void OnApplicationPause(bool pause)
+    {
+        SaveMethod();
+        if (pause) File.WriteAllText(_savingPath, json);
+    }
+#endif
+    private void OnApplicationQuit()
+    {
+        SaveMethod();
+        File.WriteAllText(_savingPath, json);
+        Debug.Log("APP QUIT gold:" + _savingInfo.Gold + " Current HP:" + _savingInfo.CurrentHP 
+            + " Max HP:" + _savingInfo.MaxHP);
+        
+    }
+
+    private void SaveMethod()
+    {
+        try
+        {
+			// Здесь пытаемся присваивоить переменные с UI экземплярам класса
+			//_savingInfo.Gold = Convert.ToInt32(playerGold.text);
+			GoldController = GameObject.Find("GoldController");
+            Monster = GameObject.Find("Chan1");
+            Upgrade = GameObject.Find("DamageController");
+            
+            _savingInfo.Gold = GoldController.GetComponent<GoldController>().Get_Player_Gold();
+            _savingInfo.CurrentHP = Monster.GetComponent<Monster>().CurrentHP;
+            _savingInfo.MaxHP = Monster.GetComponent<Monster>().MaxHP;
+            _savingInfo.boost = Upgrade.GetComponent<DamageController>()._totalBoost;
+			_savingInfo.Sprite = Monster.GetComponent<SpriteRenderer>().sprite;
+			json = JsonUtility.ToJson(_savingInfo); // преобразуем класс в json
+        }
+        catch
+        {
+            Debug.Log("FAILED TO WRITE INFO");
+            return;
+        }
+#if UNITY_ANDROID && !UNITY_EDITOR
+        _savingPath = Path.Combine(Application.persistentDataPath, "arpgsave.json"); // внешняя память -> Android -> Data -> название бандла проекта -> files
+#else
+		_savingPath = Path.Combine(Application.dataPath, "arpgsave.json");
+#endif
+    }
+}
+
+[Serializable]
+public class SavingInfo // класс для хранения сохраняемой инфы
+{
+    public int Gold; // бесплатные монетки
+    public double CurrentHP; // хп монстра на момент выхода с приложения, решил сделать одним массивом вместо двух переменных
+    public double MaxHP;
+    public int[] boost; // индекс - номер буста, значение по индексу - количество буста
+	public Sprite Sprite;
+}
